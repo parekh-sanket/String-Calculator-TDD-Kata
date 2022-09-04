1b3927f3ca067c342a53bb6f127ed774375f3572
Sanket <sanketparekh17@gmail.com>
2022-09-04 15:57:40 +0530

    unknown amount of numbers in string test

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -17,7 +17,7 @@ class test_calculator(unittest.TestCase):
         result = add("10,20")
         self.assertEqual(result, 30)
     
-    def test_unknown_amount_of_numbers(self):
+    def test_unknown_amount_of_numbers_in_string(self):
         result = add("1,2,3")
         self.assertEqual(result, 6)
         result = add("50,80,60,70")
